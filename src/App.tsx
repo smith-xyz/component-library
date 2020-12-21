a5539b75ada0f664bc5fc425dd713e7c1cecfc38
Shaun Smith <shaunsmith7@icloud.com>
2020-12-21 16:51:51 -0500

    simple data table, start on navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import DataTable, { Header } from './components/Table/DataTable'
+import './App.scss'
+
+const headers = [
+  { key: 'id', label: 'Id' },
+  { key: 'name', label: 'Name' },
+  { key: 'type', label: 'Type' },
+  { key: 'col1', label: 'col1' },
+  { key: 'col2', label: 'col2' },
+  { key: 'col3', label: 'col3' },
+  { key: 'col4', label: 'col4' },
+  { key: 'col5', label: 'col5' },
+]
+
+
+const testData = [
+  { id: 1, name: 'Philo', type: 'beagle-lab', col1: 1234, col2: 12345, col3: 12345, col4: 12345, col5: 12345 },
+  { id: 2, name: 'Henry', type: 'pitbull', col1: 1234, col2: 12345, col3: 12345, col4: 12345, col5: 12345 },
+  { id: 3, name: 'Jackson', type: 'golden-retriever', col1: 1234, col2: 12345, col3: 12345, col4: 12345, col5: 12345 },
+  { id: 4, name: 'Franky', type: 'korgy', col1: 1234, col2: 12345, col3: 12345, col4: 12345, col5: 12345 }
+]
 
 const App = () => {
   return (
     <div className="App">
-      
+      <DataTable headers={headers} rows={testData} style={ { size: "sm" }}/>
+      <DataTable headers={headers} rows={testData} style={ { size: "md" }}/>
+      <DataTable headers={headers} rows={testData} style={ { size: "lg" }}/>
     </div>
   );
 }
--- src/components/Table/DataTable.tsx
+++ src/components/Table/DataTable.tsx
@@ -1,11 +1,50 @@
 import React from 'react'
-import './Styles/base.sass'
+import './Styles/base.scss'
+import './Styles/sizes.scss'
+import './Styles/colors.scss'
 
+export interface Header {
+    key: string 
+    label?: string 
+}
 
-const DataTable = () => {
+interface TableStyle {
+    size: string
+}
+
+interface DataTableProps<T> {
+    headers: Header[]
+    rows: any
+    style: TableStyle
+}
+
+const DataTable = <T extends {[x in string | number]: any}>(props: DataTableProps<T>) => {
+    const columnHeaders = props.headers.map(h =>
+        <th key={h.key}>
+            <span>
+                {h.label ?? h.key}
+            </span>
+        </th>
+    )
+
+    const rows = props.rows.length > 0 ? props.rows.map((row: any, i: number) => {
+        return <tr key={i}>{Object.keys(row).map((k, id) => {
+            return <td key={id}>{row[k]}</td>
+        })}</tr>
+    }) : null
+
     return (
-        <div>
-
+        <div className="data-table-container">
+            <table className={props.style.size ?? ""}>
+                <thead>
+                    <tr>
+                        {columnHeaders}
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows}
+                </tbody>
+            </table>
         </div>
     )
 }
