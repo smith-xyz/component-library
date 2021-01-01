57db65090d047703d5ad99032a9d674a540be4d3
Shaun Smith <shaunsmith7@icloud.com>
2020-12-31 22:09:33 -0500

    spinner props and dynamic rendering

--- src/components/Spinners/Index.tsx
+++ src/components/Spinners/Index.tsx
@@ -1,14 +1,22 @@
-import React, { useState } from 'react'
-import Spinner from './LoadingSpinner'
+import React, { useState, useEffect } from 'react'
+import Spinner, { SpinnerProps } from './LoadingSpinner'
 
 const SpinnerIndex = () => {
+    const [color, setColor] = useState<SpinnerProps["color"]>(undefined)
+    const [size, setSize] = useState<SpinnerProps["size"]>("md")
 
     return (
         <React.Fragment>
             <div className="index-options-container">
                 <h1>Set different Props!</h1>
+                <input type="text" placeholder="Type any CSS Color" onChange={(e) => setColor(e.target.value)}></input>
+                <select value={size} onChange={e => setSize(e.target.value as SpinnerProps["size"])}>
+                    <option value="sm">sm</option>
+                    <option value="md">md</option>
+                    <option value="lg">lg</option>
+                </select>
             </div>
-            <Spinner />
+            <Spinner color={color} size={size} />
         </React.Fragment>
     )
 }
--- src/components/Spinners/LoadingSpinner.tsx
+++ src/components/Spinners/LoadingSpinner.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './Styles/base.scss'
 
-const Spinner = () => {
+export interface SpinnerProps {
+    color?: string
+    size?: "sm" | "md" | "lg"
+}
+
+const Spinner = (props: SpinnerProps) => {
+    let borderSize
+    
+    if (props.size === 'sm') borderSize = "0.25em"
+    if (props.size === 'md') borderSize = "0.5em"
+    if (props.size === 'lg') borderSize = "0.75em"
+
+    const style: React.CSSProperties = {
+        borderLeft: borderSize + " solid " + (props.color ?? "#2fc2baf5"),
+        borderBottom: borderSize + " solid " + (props.color ?? "#2fc2baf5"),
+        borderTop: borderSize + " solid " + (props.color ?? "#2fc2baf5"),
+        borderRight: borderSize + " solid transparent"
+    }
+
     return (
-        <div className="loading-spinner"></div>
+        <React.Fragment>
+            <div className={"loading-spinner " + (props.size ?? "md")} style={style}></div>
+        </React.Fragment>
     )
 }
 
